b09aff4319e66172f836372e235fb1c582d82582
nikimichael7 <michael.nikiforov7@gmail.com>
2026-07-22 16:53:21 +0100

    Updated the calculator to perform all four basic operators.

--- calculator.py
+++ calculator.py
@@ -1,8 +1,22 @@
-print("Welcome to the calculator!")
-
 num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
 
-print("The result is: " + str(num1 + num2))
+operator = input("Enter the operator (+, -, *, /): ")
+
+if operator == "+":
+    result = num1 + num2
+    print("The result is: " + str(result))
+elif operator == "-":
+    result = num1 - num2
+    print("The result is: " + str(result))
+elif operator == "*":
+    result = num1 * num2
+    print("The result is: " + str(result))
+elif operator == "/":
+    if num2 != 0:
+        result = num1 / num2
+        print("The result is: " + str(result))
+    else:
+        print("Error: Division by zero is not allowed.")
 
 
