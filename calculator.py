418a3ab552bb7bcc95f7b96c96325b8f2bb40a6a
nikimichael7 <michael.nikiforov7@gmail.com>
2026-07-22 16:51:52 +0100

    Added a calculator script that adds two numbers.

---
+++ calculator.py
@@ -0,0 +1,8 @@
+print("Welcome to the calculator!")
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+print("The result is: " + str(num1 + num2))
+
+
